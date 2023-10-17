19e6e5d8914b5f56ed7953317bec0eff11b58db9
alandrade2 <alandrade.andre@gmail.com>
2023-10-16 22:42:27 -0300

    aplicação pronta para deploy

--- src/main/java/br/com/alandrade/todolist/user/UserController.java
+++ src/main/java/br/com/alandrade/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -23,8 +25,13 @@ public ResponseEntity create(@RequestBody UserModel userModel) {
     System.out.println("Usuário já existe!");
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existente!");
   }
-  var userCreated =  this.userRepository.save(userModel);
-  return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    var passwordhashed =  BCrypt.withDefaults()
+    .hashToString(12, userModel.getPassword().toCharArray());
+
+    userModel.setPassword(passwordhashed);
+
+    var userCreated =  this.userRepository.save(userModel);
+  return ResponseEntity.status(HttpStatus.OK).body(userCreated);
 }
 
 }
